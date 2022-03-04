45ed51757c37ba30aacf342aa304f025784f8ede
vuoso-l <vuosolucas@gmail.com>
2022-03-04 08:33:50 -0300

    Update server.listen

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => console.log(`Escuchando peticiones en el puerto ${port}`));
